6c4a089c6fedbf3186d301010346980be16f4247
sheng yeong <chia.shengyeong@gmail.com>
2022-08-25 20:58:05 +0800

    optimize getting data for eventId and get all data for evenst index

--- pages/events/index.js
+++ pages/events/index.js
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 
 import EventList from '../../components/events/event-list';
 import EventsSearch from '../../components/events/events-search';
+import { getAllEvents } from '../../helpers/api-util';
 
 function AllEventsPage(props) {
   const router = useRouter();
@@ -23,41 +24,13 @@ function AllEventsPage(props) {
 }
 
 export async function getStaticProps() {
-  const response = await fetch(
-    "https://udemy-max-react-course-code-3-default-rtdb.asia-southeast1.firebasedatabase.app/events.json"
-  )
-  const data = await response.json()
-  const transformedEvents = [];  
-  
-  for (const key in data) {
-    transformedEvents.push({
-      id: key, 
-      title: data[key].title, 
-      description: data[key].description,
-      location: data[key].location,
-      date: data[key].date,
-      image: data[key].image,
-      isFeatured: data[key].isFeatured
-    })
-  }  
-
-  if (!transformedEvents) {
-    return {
-      redirect: {
-        destination: "/no-data"
-      }
-    }
-  }
-
-  if (transformedEvents.length === 0) {
-    return { notFound: true };
-  }
+  const events = await getAllEvents();
 
   return {
     props: {
-      events: transformedEvents
+      events: events
     },
-    revalidate: 10
+    revalidate: 1800
   }
 }
 
--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,8 @@ export async function getStaticProps() {
   return {
     props: {
       events: featuredEvents
-    }
+    },
+    revalidate: 1800
   }
 }
 
