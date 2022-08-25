b06d7b13f775efb6712a7ffed54ca61f0118ad34
sheng yeong <chia.shengyeong@gmail.com>
2022-08-25 16:50:23 +0800

    render events index page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,12 @@
 import { Fragment } from 'react';
 import { useRouter } from 'next/router';
 
-import { getAllEvents } from '../../dummy-data';
 import EventList from '../../components/events/event-list';
 import EventsSearch from '../../components/events/events-search';
 
-function AllEventsPage() {
+function AllEventsPage(props) {
   const router = useRouter();
-  const events = getAllEvents();
+  const {events} = props;
 
   function findEventsHandler(year, month) {
     const fullPath = `/events/${year}/${month}`;
@@ -21,6 +20,33 @@ function AllEventsPage() {
       <EventList items={events} />
     </Fragment>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://udemy-max-react-course-code-3-default-rtdb.asia-southeast1.firebasedatabase.app/events.json"
+  )
+  const data = await response.json()
+  const transformedEvents = [];  
+  
+  for (const key in data) {
+    transformedEvents.push({
+      id: key, 
+      title: data[key].title, 
+      description: data[key].description,
+      location: data[key].location,
+      date: data[key].date,
+      image: data[key].image,
+      isFeatured: data[key].isFeatured
+    })
+  }  
+
+  return {
+    props: {
+      events: transformedEvents
+    },
+    revalidate: 10
+  }
 }
 
 export default AllEventsPage;
