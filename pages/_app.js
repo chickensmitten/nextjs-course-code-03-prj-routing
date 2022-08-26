0a430ab38ac88803ab713a1ef41fb63b3ec511fe
sheng yeong <chia.shengyeong@gmail.com>
2022-08-26 11:07:58 +0800

    _document JS file and why it is needed

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,8 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
+        <title>Next Events</title>
+        <meta name="description" content="NextJS Events" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
